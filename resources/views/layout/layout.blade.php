96946413587aeb42c6b9992a9dc9a60c22e3f91a
ToanNgo1075 <0306231075@caothang.edu.vn>
2025-10-11 12:49:24 +0700

    Sửa tiêu đề

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Floral HTML CSS Template</title>
+    <title>Floral HTML CSS Mẫu</title>
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <link href="templatemo_style.css" rel="stylesheet" type="text/css" />
